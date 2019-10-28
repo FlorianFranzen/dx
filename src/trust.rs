9e71d885d03ed650edbec9c8d7e6686f6f29d243
Florian Franzen <Florian.Franzen@gmail.com>
2019-10-28 01:08:38 +0100

    trust: Load private key

--- src/trust.rs
+++ src/trust.rs
@@ -18,6 +18,7 @@ use libp2p::{
 pub struct TrustedIdentity {
     pub name: String,
     public: PublicKey,
+    private: Option<Keypair>,
 }
 
 impl TrustedIdentity {
@@ -33,8 +34,9 @@ impl TrustedIdentity {
         fs::write(prefix.with_extension("pub"), key.public().encode()).unwrap();
 
         let public = PublicKey::Ed25519(key.public());
+        let private = Some(Keypair::Ed25519(key));
 
-        TrustedIdentity { name, public }
+        TrustedIdentity { name, public, private }
     }
 
     /// Load an excisting identity from .pub file
@@ -43,15 +45,32 @@ impl TrustedIdentity {
         let key = ed25519::PublicKey::decode(&data).unwrap();
         let public = PublicKey::Ed25519(key);
 
+        let private = if let Ok(mut data) = fs::read(file.with_extension("key")) {
+            let key = ed25519::Keypair::decode(data.as_mut_slice()).unwrap();
+            Some(Keypair::Ed25519(key))
+        } else {
+            None
+        };
+
+        //let private = fs::read(file.with_extension("key")).as_mut()
+        //    .and_then(Vec::as_mut_slice)
+        //    .and_then(ed25519::Keypair::decode)
+        //    .and_then(Option::unwrap)
+        //    .and_then(Keypair::Ed25519).ok();
+
         let name = file.file_stem().unwrap()
             .to_owned().into_string().unwrap();
 
-        TrustedIdentity{ name, public }
+        TrustedIdentity{ name, public, private }
     }
 
     /// Compute peer id from identity
     pub fn id(&self) -> PeerId {
         PeerId::from_public_key(self.public.clone())
+    }
+
+    pub fn key(&self) -> Keypair {
+        self.private.clone().expect("Missing private key.")
     }
 }
 
@@ -81,6 +100,16 @@ impl TrustStore {
         }
 
         TrustStore{ids}
+    }
+
+    pub fn find(&self, name: &str) -> Option<&TrustedIdentity> {
+        for id in self.ids.iter() {
+            if id.name == name {
+                return Some(id)
+            }
+        }
+
+        None
     }
 
 }
